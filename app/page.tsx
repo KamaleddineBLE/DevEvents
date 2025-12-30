1cfda1c8b66baad81a68b0efc8bc2ce98ac02a81
KamaleddineBLE <kamalboulenache@gmail.com>
2025-12-30 17:19:12 +0100

    test connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,36 @@
-import React from 'react'
 import ExploreBtn from './components/ExploreBtn'
 import EventCard from './components/EventCard'
-import { IEvent } from '@/database';
-import { cacheLife } from 'next/cache';
-const page = async () => {
-  'use cache';
-  cacheLife('hours')
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events`, {cache:'no-store'});
-  const {events} = await response.json();
-  return (
-    <section>
-      <h1 className='text-center'>the Hub for Everydev <br/> Events You Cant't Miss</h1>
-      <p className='text-center mt-5 '>Hackathons,Meetups,and Conferences ,All in one place</p>
-      <ExploreBtn/>
-      <div className="mt-20 space-y-7">
-        <h3>Featured Events</h3>
-        <ul className='events' >
-          {events && events.length > 0 && events.map((event:IEvent)=>(
-            <li key={event.title} className='list-none' >
-              <EventCard {...event}/></li>
-            ))}
-        </ul>
+import {IEvent} from "@/database";
+import {cacheLife} from "next/cache";
 
-      </div>
-  
-    </section>
-    
-  )
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+    'use cache';
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
+
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+            <ExploreBtn />
+
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
 }
 
-export default page
+export default Page;
